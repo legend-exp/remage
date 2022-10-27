8e48c883b9aa2d2e1f66e68f090a9b507e125633
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-10-27 15:03:49 +0200

    [tests] add native volume sampling validation

--- tests/confinement/gdml/geometry.py
+++ tests/confinement/gdml/geometry.py
@@ -1,3 +1,4 @@
+from math import pi
 import numpy as np
 from pyg4ometry import gdml, geant4, stl
 from pyg4ometry import visualisation as vis
@@ -16,12 +17,22 @@ reg.setWorld(world_l)
 # box
 box_s = geant4.solid.Box("Box", 1, 1, 1, reg, lunit="m")
 box_l = geant4.LogicalVolume(box_s, "G4_Pb", "Box", reg)
-geant4.PhysicalVolume([0, 0, 0], [2, 0, 0, "m"], box_l, "Box", world_l, reg)
+geant4.PhysicalVolume([pi/4, -pi/4, pi/4], [2, 0, 0, "m"], box_l, "Box", world_l, reg)
 
-# sphere
-sphere_s = geant4.solid.Orb("Sphere", 0.5, reg, lunit="m")
+# orb
+orb_s = geant4.solid.Orb("Orb", 0.5, reg, lunit="m")
+orb_l = geant4.LogicalVolume(orb_s, "G4_Pb", "Orb", reg)
+geant4.PhysicalVolume([0, 0, 0], [0, 2, 0, "m"], orb_l, "Orb", world_l, reg)
+
+# spherical thing
+sphere_s = geant4.solid.Sphere("Sphere", 0.2, 0.4, pi/3, 2*pi/3, pi/6, pi/2, reg, lunit="m")
 sphere_l = geant4.LogicalVolume(sphere_s, "G4_Pb", "Sphere", reg)
-geant4.PhysicalVolume([0, 0, 0], [0, 0, 0, "m"], sphere_l, "Sphere", world_l, reg)
+geant4.PhysicalVolume([pi/4, pi/4, -pi/4], [0, 0, 2, "m"], sphere_l, "Sphere", world_l, reg)
+
+# tubs
+tubs_s = geant4.solid.Tubs("Tubs", 0.2, 0.5, 1.2, pi/3, 4*pi/3, reg, lunit="m")
+tubs_l = geant4.LogicalVolume(tubs_s, "G4_Pb", "Tubs", reg)
+geant4.PhysicalVolume([-pi/4, pi/4, pi/4], [-2, 0, 0, "m"], tubs_l, "Tubs", world_l, reg)
 
 world_l.checkOverlaps(recursive=True)
 
