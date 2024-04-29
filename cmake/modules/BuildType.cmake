689a9c44a2958e55deb579863879fc73456b2aee
Manuel Huber <ManuelHu@users.noreply.github.com>
2024-04-29 14:33:44 +0200

    Add ASAN build

--- cmake/modules/BuildType.cmake
+++ cmake/modules/BuildType.cmake
@@ -8,8 +8,19 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
       "${default_build_type}"
       CACHE STRING "Choose the type of build." FORCE)
   # Set the possible values of build type for cmake-gui
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel"
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "ASAN" "Debug" "Release" "MinSizeRel"
                                                "RelWithDebInfo")
 else()
   message(STATUS "Setting build type to '${CMAKE_BUILD_TYPE}'")
 endif()
+
+# build flags for special ASAN build.
+set(CMAKE_C_FLAGS_ASAN "${CMAKE_C_FLAGS_DEBUG} -fsanitize=address,undefined -fno-omit-frame-pointer")
+
+set(CMAKE_CXX_FLAGS_ASAN "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=address,undefined -fno-omit-frame-pointer")
+
+set(CMAKE_EXE_LINKER_FLAGS_ASAN
+    "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -fsanitize=address,undefined -static-libasan")
+
+set(CMAKE_SHARED_LINKER_FLAGS_ASAN
+    "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fsanitize=address,undefined -static-libasan")
