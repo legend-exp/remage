2fcc79a9a5ead96a4d25a505b24c2630abe30edd
Manuel Huber <info@manuelhu.de>
2024-08-09 23:30:40 +0200

    tests: also test root output writing output files

---
+++ tests/output/run-test-root.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+rmg="$1"
+macro="macros/$2"
+output="${2/.mac/.root}"
+
+# run remage, produce root output.
+"$rmg" -g gdml/geometry.gdml -o "$output" -- "$macro"
+
+# check that output file is there.
+test -f "$output"
