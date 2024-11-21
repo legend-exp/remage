d95171a9b88cde9cee0f8cf7e60dba698e665efc
EricMEsch <eric.esch@student.uni-tuebingen.de>
2024-11-21 15:47:24 +0100

    Adjust examples to the new detector handling

--- examples/03-optics/main.cc
+++ examples/03-optics/main.cc
@@ -8,8 +8,8 @@ int main(int argc, char** argv) {
 
   RMGManager manager("03-optics", argc, argv);
   manager.GetDetectorConstruction()->IncludeGDMLFile("gdml/geometry.gdml");
-  manager.GetDetectorConstruction()->RegisterDetector(RMGHardware::kOptical, "Detector1", 0);
-  manager.GetDetectorConstruction()->RegisterDetector(RMGHardware::kOptical, "Detector2", 1);
+  manager.GetDetectorConstruction()->RegisterDetector(kOptical, "Detector1", 0);
+  manager.GetDetectorConstruction()->RegisterDetector(kOptical, "Detector2", 1);
 
   std::string macro = argc > 1 ? argv[1] : "";
   if (!macro.empty()) manager.IncludeMacroFile(macro);
--- examples/06-NeutronCapture/main.cc
+++ examples/06-NeutronCapture/main.cc
@@ -15,10 +15,10 @@ int main(int argc, char** argv) {
   user_init->AddOptionalOutputScheme<IsotopeOutputScheme>("IsotopeOutputScheme");
 
   // Need at least 1 active detector, otherwise persistency not enabled (even if manually setting it)
-  manager.GetDetectorConstruction()->RegisterDetector(RMGHardware::kGermanium, "HPGe1", 0);
-  // manager.GetDetectorConstruction()->RegisterDetector(RMGHardware::kGermanium, "HPGe2", 1);
-  // manager.GetDetectorConstruction()->RegisterDetector(RMGHardware::kGermanium, "HPGe3", 2);
-  // manager.GetDetectorConstruction()->RegisterDetector(RMGHardware::kGermanium, "HPGe4", 3);
+  manager.GetDetectorConstruction()->RegisterDetector(kGermanium, "HPGe1", 0);
+  // manager.GetDetectorConstruction()->RegisterDetector(kGermanium, "HPGe2", 1);
+  // manager.GetDetectorConstruction()->RegisterDetector(kGermanium, "HPGe3", 2);
+  // manager.GetDetectorConstruction()->RegisterDetector(kGermanium, "HPGe4", 3);
 
   std::string macro = argc > 1 ? argv[1] : "";
   if (!macro.empty()) manager.IncludeMacroFile(macro);
