4ceeb2f52e8f9f2ef82ce9f938612778c17d24f9
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2024-12-28 20:17:04 +0100

    just sys.argv instead of argparse and a bug fix

--- python/remage/cli.py
+++ python/remage/cli.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import argparse
 import subprocess
 import sys
 
@@ -8,20 +7,5 @@ from .cpp_config import REMAGE_CPP_EXE_PATH
 
 
 def remage_cli():
-    parser = argparse.ArgumentParser(
-        prog="remage", description="remage's command-line interface"
-    )
-
-    parser.add_argument(
-        "--version", action="store_true", help="""Print remage version and exit"""
-    )
-
-    args = parser.parse_args()
-
-    cmdline = [REMAGE_CPP_EXE_PATH]
-    if args.version:
-        cmdline += ["--version"]
-
-    result = subprocess.run(cmdline, check=False)
-
+    result = subprocess.run([REMAGE_CPP_EXE_PATH] + sys.argv[1:], check=False)
     sys.exit(result.returncode)
--- python/remage/cpp_config.py
+++ python/remage/cpp_config.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-REMAGE_CPP_EXE_PATH = "/home/gipert/sw/src/legend/remage/build/install/bin/remage-cpp"
+REMAGE_CPP_EXE_PATH = "/home/gipert/sw/src/legend/remage/build/src/remage-cpp"
