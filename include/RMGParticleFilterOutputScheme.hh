2296c90f91fd6d248cd80604dd7a097be126f646
EricMEsch <eric.esch@student.uni-tuebingen.de>
2025-01-27 15:54:11 +0100

    Add Particle filter stacking action option

---
+++ include/RMGParticleFilterOutputScheme.hh
@@ -0,0 +1,49 @@
+// Copyright (C) 2022 Luigi Pertoldi <gipert@pm.me>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef _RMG_PARTICLE_FILTER_OUTPUT_SCHEME_HH_
+#define _RMG_PARTICLE_FILTER_OUTPUT_SCHEME_HH_
+
+#include <optional>
+#include <set>
+
+#include "G4AnalysisManager.hh"
+#include "G4GenericMessenger.hh"
+#include "G4VUserEventInformation.hh"
+
+#include "RMGVOutputScheme.hh"
+
+class G4Event;
+class RMGParticleFilterOutputScheme : public RMGVOutputScheme {
+
+  public:
+
+    RMGParticleFilterOutputScheme();
+
+    std::optional<G4ClassificationOfNewTrack> StackingActionClassify(const G4Track*, int) override;
+
+    inline void AddParticle(int pdg) { fParticles.insert(pdg); }
+
+  private:
+
+    std::unique_ptr<G4GenericMessenger> fMessenger;
+    void DefineCommands();
+
+    std::set<int> fParticles;
+};
+
+#endif
+
+// vim: tabstop=2 shiftwidth=2 expandtab
