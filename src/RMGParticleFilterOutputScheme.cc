2c5c34fe3b20612812675afb8ec0510caf43137b
EricMEsch <eric.esch@student.uni-tuebingen.de>
2025-02-08 16:15:59 +0100

    Add option to specify kill/keep volumes

--- src/RMGParticleFilterOutputScheme.cc
+++ src/RMGParticleFilterOutputScheme.cc
@@ -24,13 +24,45 @@
 
 RMGParticleFilterOutputScheme::RMGParticleFilterOutputScheme() { this->DefineCommands(); }
 
+void RMGParticleFilterOutputScheme::AddKeepVolume(std::string name) {
+  if (!fKillVolumes.empty()) {
+    RMGLog::OutDev(RMGLog::error, "Conflicting requests for kill/keep volume in ParticleFilter. "
+                                  "Trying to assign keep-volume but a kill-volume already exists.");
+  }
+
+  fKeepVolumes.insert(name);
+}
+
+void RMGParticleFilterOutputScheme::AddKillVolume(std::string name) {
+  if (!fKeepVolumes.empty()) {
+    RMGLog::OutDev(RMGLog::error, "Conflicting requests for kill/keep volume in ParticleFilter. "
+                                  "Trying to assign kill-volume but a keep-volume already exists.");
+  }
+
+  fKillVolumes.insert(name);
+}
 
 std::optional<G4ClassificationOfNewTrack> RMGParticleFilterOutputScheme::
     StackingActionClassify(const G4Track* aTrack, int stage) {
 
-  int pdg = aTrack->GetDefinition()->GetPDGEncoding();
+  const int pdg = aTrack->GetDefinition()->GetPDGEncoding();
+  // If the particle is not marked to kill, let it go
   if (fParticles.find(pdg) == fParticles.end()) return std::nullopt;
 
+  const auto pv = aTrack->GetTouchableHandle()->GetVolume();
+  // I am not 100% sure the physical volume will exist at this point. Can remove after some tests
+  if (!pv) { RMGLog::OutDev(RMGLog::debug, "Physical volume does not exist in ParticleFilter!"); }
+  const auto pv_name = pv->GetName();
+  // If a kill volume is specified only kill if in the kill volume.
+  if (!fKillVolumes.empty() && fKillVolumes.find(pv_name) == fKillVolumes.end())
+    return std::nullopt;
+  // If a keep volume is specified only keep particle if in that volume.
+  if (fKeepVolumes.find(pv_name) != fKeepVolumes.end()) return std::nullopt;
+
+  // We land here if
+  // i) Particle is marked to kill.
+  // ii) No Kill volume specified or the particle is in the kill volume.
+  // iii) Particle is not in the keep volume.
   RMGLog::OutDev(RMGLog::debug, "Filtering out particle with PDG code ", pdg,
       " in RMGParticleFilterOutputScheme");
   return fKill;
@@ -45,6 +77,18 @@ void RMGParticleFilterOutputScheme::DefineCommands() {
       .SetGuidance("Add a particle to be filtered out by its PDG code. User is responsible for "
                    "correct PDG code.")
       .SetParameterName(0, "PDGcode", false, false)
+      .SetStates(G4State_Idle);
+
+  fMessenger->DeclareMethod("AddKeepVolume", &RMGParticleFilterOutputScheme::AddKeepVolume)
+      .SetGuidance("Add a physical volume by name in which all specified Particles will be kept. "
+                   "They will be killed everywhere else. Can NOT be mixed with KillVolumes.")
+      .SetParameterName(0, "PhysicalVolumeName", false, false)
+      .SetStates(G4State_Idle);
+
+  fMessenger->DeclareMethod("AddKillVolume", &RMGParticleFilterOutputScheme::AddKillVolume)
+      .SetGuidance("Add a physical volume by name in which all specified Particles will be killed. "
+                   "They will only be killed in this volume. Can NOT be mixed with KeepVolumes.")
+      .SetParameterName(0, "PhysicalVolumeName", false, false)
       .SetStates(G4State_Idle);
 }
 
