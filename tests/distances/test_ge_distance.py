98ae00cb3c34dbe8d5dbe3ecbd53c30aea64e233
Toby Dixon <tdixon@berkeley.edu>
2025-02-23 17:41:09 +0100

    more tests for distances
    style: pre-commit fixes

--- tests/distances/test_ge_distance.py
+++ tests/distances/test_ge_distance.py
@@ -6,12 +6,12 @@
 from __future__ import annotations
 
 import copy
+import sys
 
 import awkward as ak
 import legendhpges as hpges
 import numpy as np
 import pyg4ometry as pg4
-import sys
 from legendhpges import draw
 from lgdo import lh5
 from matplotlib import pyplot as plt
@@ -21,7 +21,7 @@ plt.rcParams["lines.linewidth"] = 1
 plt.rcParams["font.size"] = 12
 
 gdml = "gdml/ge-array.gdml"
-outfile =sys.argv[1]
+outfile = sys.argv[1]
 
 # get the geometry
 reg = pg4.gdml.Reader(gdml).getRegistry()
@@ -95,8 +95,15 @@ def make_plot(hit, tolerance=1e-9):
         coords = np.column_stack((sel_hit.xloc, sel_hit.yloc, sel_hit.zloc))
         dist_py = det_map[det]["hpge"].distance_to_surface(coords)
         # Check if all distances are within tolerance
+
         if np.sum(np.abs(sel_hit.dist_G4 - dist_py) > tolerance):
             good_distance = False
+
+            bad = np.where(np.abs(sel_hit.dist_G4 - dist_py) > tolerance)
+            msg = "The following distances are difference\n"
+            msg += f"Geant4 {sel_hit.dist_G4[bad]} \n"
+            msg += f"Python {dist_py[bad]}"
+            print(msg)
 
         # Now draw the plots
         draw.plot_profile(det_map[det]["hpge"], axes=axs[idx])
@@ -147,5 +154,6 @@ plt.figtext(0.02, 0.04, caption, wrap=True, ha="left", fontsize=11)
 plt.tight_layout(rect=[0, 0.12, 1, 1])
 # plt.tight_layout()
 plt.savefig("distance-ge.output.pdf")
+
 
 assert are_distances_good
