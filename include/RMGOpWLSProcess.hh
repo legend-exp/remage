d3408380b024e9ea412be7c954b305ce72f4efb8
Manuel Huber <info@manuelhu.de>
2025-02-25 14:20:14 +0100

    update some copyright notices for myself [skip ci]

--- include/RMGOpWLSProcess.hh
+++ include/RMGOpWLSProcess.hh
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 Luigi Pertoldi <gipert@pm.me>
+// Copyright (C) 2024 Manuel Huber
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the Free
