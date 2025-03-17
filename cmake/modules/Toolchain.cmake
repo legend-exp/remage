df3d4a0352bc9a71e4eab786807d8af6e5db48ee
Manuel Huber <ManuelHu@users.noreply.github.com>
2025-03-17 23:37:29 +0100

    Update Toolchain.cmake [skip ci]

--- cmake/modules/Toolchain.cmake
+++ cmake/modules/Toolchain.cmake
@@ -1,10 +1,10 @@
 # configure useful post-install scripts
 
-macro(create_mage_toolchain)
+function(create_remage_toolchain)
   # create and eventually install configuration scripts
   configure_file(${PROJECT_SOURCE_DIR}/cmake/project-config.in
                  ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}-config @ONLY)
 
   install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}-config
           DESTINATION ${CMAKE_INSTALL_BINDIR})
-endmacro()
+endfunction()
