6430d114bfaaa7e18805a4a9ae6c26c8d1735082
Toby Dixon <tdixon@berkeley.edu>
2025-03-22 21:23:02 +0100

    adding the simulation

---
+++ tests/observables/run_sim.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+
+import subprocess
+import sys
+from pathlib import Path
+
+rmg = sys.argv[1]
+
+
+def replace_lines(
+    input_file: str, output_file: str, replacements: dict[str, str]
+) -> None:
+    """Replaces lines in a file that match given patterns.
+
+    Parameters:
+    - input_file: Path to the input text file.
+    - output_file: Path to save the modified text file.
+    - replacements: A dictionary where keys are regex patterns to match lines,
+      and values are the replacement strings.
+    """
+    with Path(input_file).open(encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with Path(output_file).open("w", encoding="utf-8") as f:
+        for line in lines:
+            line_t = line
+            for pattern, replacement in replacements.items():
+                if pattern in line:
+                    line_t = replacement + "\n"
+                    break
+            f.write(line_t)
+
+
+def run_sim(
+    generator_name="",
+    name="",
+    val="0",
+    step_limits="",
+    prod_cuts="",
+    step_points="",
+    proc="",
+    generator="",
+    register_lar=False,
+):
+    macro_file = "mac.mac"
+    dir_string = f"{generator_name}/{name}/max_{val}/"
+
+    # make the out directory
+    stp_directory = Path(f"out/{dir_string}/stp/")
+    macro_directory = Path(f"macros/{dir_string}/")
+
+    lar_command = (
+        "/RMG/Geometry/RegisterDetector Scintillator LAr 002" if register_lar else ""
+    )
+
+    stp_directory.mkdir(parents=True, exist_ok=True)
+    macro_directory.mkdir(parents=True, exist_ok=True)
+
+    replacements = {
+        "$STEP_LIMITS_COMMAND": step_limits,
+        "$PROD_CUTS_COMMAND": prod_cuts,
+        "$GENERATOR": generator,
+        "$STEP_POINT": step_points,
+        "$PROC": proc,
+        "$REGISTER_LAR": lar_command,
+    }
+    replace_lines(
+        "macros/template.mac", macro_directory / Path(macro_file), replacements
+    )
+    subprocess.run(
+        f"{rmg} {macro_directory / macro_file} -g gdml/geometry.gdml -o {stp_directory}/out.lh5 -w -t 1  ",
+        shell=True,
+        check=False,
+    )
+
+
+do_bulk = True
+do_surf = True
+energy = 1000
+
+generators = {}
+cuts = [10, 20, 50, 100, 200, None]
+
+
+# define some generator commands
+if do_surf:
+    generators["beta_surf"] = f"""
+/RMG/Generator/Select GPS
+/gps/position 0 0 -20 mm
+/gps/particle e-
+/gps/energy {energy} keV
+/gps/direction 0 0 1
+"""
+
+if do_bulk:
+    generators["beta_bulk"] = f"""
+/RMG/Generator/Confine Volume
+/RMG/Generator/Confinement/Physical/AddVolume germanium
+/RMG/Generator/Select GPS
+/gps/particle e-
+/gps/ang/type iso
+/gps/energy {energy} keV
+"""
+
+
+# with and without the argon table
+profile = {}
+for generator, config in generators.items():
+    profile[generator] = {}
+
+    # loop over step limits
+    for step_limits in cuts:
+        command = (
+            f"/RMG/Geometry/SetMaxStepSize {step_limits} um germanium"
+            if step_limits is not None
+            else ""
+        )
+        run_sim(
+            generator_name=generator,
+            name="step_limits",
+            val=step_limits,
+            step_limits=command,
+            prod_cuts="",
+            proc="",
+            step_points="/RMG/Output/Germanium/StepPositionMode Both",
+            generator=config,
+            register_lar=False,
+        )
