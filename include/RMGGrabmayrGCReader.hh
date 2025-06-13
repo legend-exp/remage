08d57822acfbeb07a7a0044ab43b6a1f8c58c87b
Luigi Pertoldi <gipert@pm.me>
2025-06-13 12:58:47 +0200

    Replace emails with ORCID links
    Co-authored-by: Eric Esch <124439591+EricMEsch@users.noreply.github.com>

--- include/RMGGrabmayrGCReader.hh
+++ include/RMGGrabmayrGCReader.hh
@@ -1,4 +1,5 @@
-// Copyright (C) 2022 Luigi Pertoldi <gipert@pm.me>
+// Copyright (C) 2024 Moritz Neuberger <https://orcid.org/0009-0001-8471-9076>
+// Copyright (C) 2024 Eric Esch <https://orcid.org/0009-0000-4920-9313>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the Free
--- src/RMGGrabmayrGCReader.cc
+++ src/RMGGrabmayrGCReader.cc
@@ -1,3 +1,20 @@
+// Copyright (C) 2024 Moritz Neuberger <https://orcid.org/0009-0001-8471-9076>
+// Copyright (C) 2024 Eric Esch <https://orcid.org/0009-0000-4920-9313>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 #include "RMGGrabmayrGCReader.hh"
 
 #include "G4Tokenizer.hh"
