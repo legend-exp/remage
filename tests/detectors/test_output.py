91d241679d775011ab8a4cc9e3988b2dcabae97d
EricMEsch <eric.esch@student.uni-tuebingen.de>
2025-07-31 14:54:50 +0200

    Change test to only check if the output fields exist

--- tests/detectors/test_output.py
+++ tests/detectors/test_output.py
@@ -1,50 +1,32 @@
 from __future__ import annotations
 
-import difflib
-import re
-import subprocess
-from pathlib import Path
-
-
-def strip_ansi(text: str) -> str:
-    return re.sub(r"\x1B\[[0-9;]*[mK]", "", text)
-
-
-def normalize_lines(s):
-    # Remove trailing whitespace from each line
-    return [line.rstrip() for line in s.strip().splitlines()]
-
+from lgdo import lh5
 
 output_files = [
-    "det-from-gdml.lh5",
-    "det-from-regex.lh5",
-    "det-from-name.lh5",
-    "det-from-both.lh5",
+    ("gdml", "det-from-gdml.lh5"),
+    ("regex", "det-from-regex.lh5"),
+    ("name", "det-from-name.lh5"),
 ]
 
-dump_files = [
-    "dumps/register-germanium.lh5.ls",
-    "dumps/register-germanium.lh5.ls",
-    "dumps/register-germanium.lh5.ls",
-    "dumps/register-all.lh5.ls",
-]
-
-for output_file, dump_file in zip(output_files, dump_files):
-    result = subprocess.run(
-        ["lh5ls", str(output_file)], capture_output=True, text=True, check=True
-    )
-    actual_output = normalize_lines(strip_ansi(result.stdout))
-    expected_output = normalize_lines(Path(dump_file).read_text())
-
-    if actual_output != expected_output:
-        diff = "\n".join(
-            difflib.unified_diff(
-                expected_output,
-                actual_output,
-                fromfile=dump_file,
-                tofile=output_file,
-                lineterm="",
-            )
+# Check if the output rows exist
+for registering_type, output_file in output_files:
+    try:
+        lh5.read_as("/stp/germanium_det1", output_file, "ak")
+        lh5.read_as("/stp/germanium_det2", output_file, "ak")
+    except Exception as err:
+        msg = (
+            f"Expected output rows for '{output_file}' do not exist. "
+            f"Detectors not registered correctly from {registering_type}."
         )
-        msg = f"Mismatch in {output_file}:\n{diff}"
-        raise AssertionError(msg)
+        raise AssertionError(msg) from err
+
+try:
+    lh5.read_as("/stp/germanium_det1", "det-from-both.lh5", "ak")
+    lh5.read_as("/stp/germanium_det2", "det-from-both.lh5", "ak")
+    lh5.read_as("/stp/different_det", "det-from-both.lh5", "ak")
+except Exception as err:
+    msg = (
+        "Expected output rows for 'det-from-both.lh5' do not exist. "
+        "Detectors not registered correctly when using both functions."
+    )
+    raise AssertionError(msg) from err
