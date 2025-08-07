9a5f44f0f1984df123233998911590a261025e57
EricMEsch <eric.esch@student.uni-tuebingen.de>
2025-08-07 17:02:05 +0200

    Add plot function

--- tests/bxdecay/run_sims.py
+++ tests/bxdecay/run_sims.py
@@ -5,7 +5,7 @@ from remage import remage_run
 bbv0_commands = [
     "macros/template.mac",
     "/RMG/Generator/BxDecay0/DoubleBetaDecay Ge76 0vbb",
-    "/run/beamOn 100",
+    "/run/beamOn 10000",
 ]
 # the two template macros are just to incorporate a test that makes sure the order of vertex macros does not matter
 bbv2_commands = [
