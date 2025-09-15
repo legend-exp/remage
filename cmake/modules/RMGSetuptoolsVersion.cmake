3536e5f72322739277b8670976d30de23c684d35
Manuel Huber <ManuelHu@users.noreply.github.com>
2025-09-15 23:36:44 +0200

    cmake: add option RMG_VERSION_SCM_USE_VENV

--- cmake/modules/RMGSetuptoolsVersion.cmake
+++ cmake/modules/RMGSetuptoolsVersion.cmake
@@ -1,17 +1,24 @@
 # create the virtual environment with python-venv and install setuptools-scm
-set(SCM_VENV_DIR ${CMAKE_BINARY_DIR}/scm_venv)
+option(RMG_VERSION_SCM_USE_VENV "use venv and uv for setuptools-scm" ON)
 
-if(NOT EXISTS "${SCM_VENV_DIR}/initialized")
-  execute_process(COMMAND ${Python3_EXECUTABLE} -m venv "${SCM_VENV_DIR}" COMMAND_ERROR_IS_FATAL
-                          ANY)
-  execute_process(COMMAND ${SCM_VENV_DIR}/bin/python -m pip -q install --no-warn-script-location
-                          --upgrade pip setuptools setuptools-scm COMMAND_ERROR_IS_FATAL ANY)
-  file(TOUCH "${SCM_VENV_DIR}/initialized")
+set(_scm_python_cmd "python")
+
+if(RMG_VERSION_SCM_USE_VENV)
+  set(SCM_VENV_DIR ${CMAKE_BINARY_DIR}/scm_venv)
+
+  if(NOT EXISTS "${SCM_VENV_DIR}/initialized")
+    execute_process(COMMAND ${Python3_EXECUTABLE} -m venv "${SCM_VENV_DIR}" COMMAND_ERROR_IS_FATAL
+                            ANY)
+    execute_process(COMMAND ${SCM_VENV_DIR}/bin/python -m pip -q install --no-warn-script-location
+                            --upgrade pip setuptools setuptools-scm COMMAND_ERROR_IS_FATAL ANY)
+    file(TOUCH "${SCM_VENV_DIR}/initialized")
+  endif()
+  set(_scm_python_cmd "${SCM_VENV_DIR}/bin/python")
 endif()
 
 # try to get the version from setuptools-scm.
 execute_process(
-  COMMAND ${SCM_VENV_DIR}/bin/python -m setuptools_scm
+  COMMAND ${_scm_python_cmd} -m setuptools_scm
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
   OUTPUT_VARIABLE GIT_OUTPUT
   RESULT_VARIABLE GIT_STATUS
