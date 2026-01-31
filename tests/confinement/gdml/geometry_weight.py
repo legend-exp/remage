3545dd33c9cc774454f97a00bf2dcf7b5f96aec6
Manuel Huber <info@manuelhu.de>
2026-01-31 12:20:15 +0100

    tests: add tests for the isotope sampling mode

--- tests/confinement/gdml/geometry_weight.py
+++ tests/confinement/gdml/geometry_weight.py
@@ -6,21 +6,38 @@ from pyg4ometry import gdml, geant4
 
 reg = geant4.Registry()
 
-world_side = 10  # m
+# dummy isotopes/elements with numbers that are easy to calculate.
+iso1 = geant4.Isotope("isotope1", 32, 76, 76, reg)
+iso2 = geant4.Isotope("isotope2", 32, 75, 75, reg)
+
+el1 = geant4.ElementIsotopeMixture("element1", "X", 2, reg)
+el1.add_isotope(iso1, 0.5)
+el1.add_isotope(iso2, 0.5)
+
+el2 = geant4.ElementIsotopeMixture("element2", "X", 2, reg)
+el2.add_isotope(iso1, 0.25)
+el2.add_isotope(iso2, 0.75)
+
+mat1 = geant4.MaterialCompound("mat1", 10, 1, reg)
+mat1.add_element_massfraction(el1, 1)
+
+mat2 = geant4.MaterialCompound("mat2", 5, 1, reg)
+mat2.add_element_massfraction(el2, 1)
 
 # world volume (air)
+world_side = 10  # m
 world_s = geant4.solid.Box("World", world_side, world_side, world_side, reg, lunit="m")
 world_l = geant4.LogicalVolume(world_s, "G4_Pb", "World", reg)
 reg.setWorld(world_l)
 
 # 2 boxes with one smaller
 box_s = geant4.solid.Box("Box", 1, 1, 1, reg, lunit="m")
-box1_l = geant4.LogicalVolume(box_s, "G4_Pb", "Box1", reg)
+box1_l = geant4.LogicalVolume(box_s, mat1, "Box1", reg)
 geant4.PhysicalVolume(
     [pi / 4, -pi / 4, pi / 4], [2, 0, 0, "m"], box1_l, "Box1", world_l, reg
 )
 
-box2_l = geant4.LogicalVolume(box_s, "G4_Be", "Box2", reg)
+box2_l = geant4.LogicalVolume(box_s, mat2, "Box2", reg)
 geant4.PhysicalVolume(
     [pi / 4, -pi / 4, pi / 4], [-2, 0, 0, "m"], box2_l, "Box2", world_l, reg
 )
