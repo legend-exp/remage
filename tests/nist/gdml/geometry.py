8781a5dce8b4ff211ffa525db8e14e15f776dc73
Manuel Huber <info@manuelhu.de>
2026-02-11 16:02:04 +0100

    tests: update nist test to include more materials

--- tests/nist/gdml/geometry.py
+++ tests/nist/gdml/geometry.py
@@ -2,23 +2,34 @@ from __future__ import annotations
 
 import pyg4ometry as pg4
 import pygeomhpges as hpges
+import pygeomtools
 
-reg = pg4.geant4.Registry()
+for mat_str in ("ge", "ar", "cu"):
+    reg = pg4.geant4.Registry()
 
-# create a world volume
-world_s = pg4.geant4.solid.Orb("world", 20, registry=reg, lunit="cm")
-world_l = pg4.geant4.LogicalVolume(world_s, "G4_Galactic", "world", registry=reg)
-reg.setWorld(world_l)
+    # create a world volume
+    world_s = pg4.geant4.solid.Orb("world", 20, registry=reg, lunit="cm")
+    world_l = pg4.geant4.LogicalVolume(world_s, "G4_Galactic", "world", registry=reg)
+    reg.setWorld(world_l)
 
-# let's make a germanium balloon
-natge = hpges.materials.make_natural_germanium(reg)
+    # let's make a germanium balloon
+    if mat_str == "ge":
+        mat = hpges.materials.make_natural_germanium(reg)
+    elif mat_str == "ar":
+        mat = pygeomtools.materials.LegendMaterialRegistry(
+            reg, enable_optical=False
+        ).liquidargon
+    elif mat_str == "cu":
+        mat = pygeomtools.materials.LegendMaterialRegistry(
+            reg, enable_optical=False
+        ).metal_copper
 
-ge_s = pg4.geant4.solid.Orb("germanium", 15, registry=reg, lunit="cm")
-ge_l = pg4.geant4.LogicalVolume(ge_s, natge, "germanium", registry=reg)
-pg4.geant4.PhysicalVolume(
-    [0, 0, 0], [0, 0, 0], ge_l, "germanium", world_l, registry=reg
-)
+    ge_s = pg4.geant4.solid.Orb("target", 15, registry=reg, lunit="cm")
+    ge_l = pg4.geant4.LogicalVolume(ge_s, mat, "target", registry=reg)
+    pg4.geant4.PhysicalVolume(
+        [0, 0, 0], [0, 0, 0], ge_l, "target", world_l, registry=reg
+    )
 
-w = pg4.gdml.Writer()
-w.addDetector(reg)
-w.write("geometry.gdml")
+    w = pg4.gdml.Writer()
+    w.addDetector(reg)
+    w.write(f"geometry-{mat_str}.gdml")
