0f15c7ded47d014413589978a049672cab30ac27
Manuel Huber <info@manuelhu.de>
2026-03-06 17:06:08 +0100

    tests: update nist validation

--- tests/nist/test_electron_interactions.py
+++ tests/nist/test_electron_interactions.py
@@ -4,18 +4,16 @@ from pathlib import Path
 
 import awkward as ak
 import hist
+import matplotlib.colors
 import matplotlib.pyplot as plt
 import numpy as np
 from lgdo import lh5
 
 # simulated energies in keV
-energies = np.concatenate(
-    [
-        np.arange(30, 100, step=20),
-        np.arange(100, 500, step=200),
-        np.arange(500, 5000, step=500),
-    ]
-)
+energies = np.geomspace(30, 50000, num=15).astype(int)  # in keV
+step_sizes = (1, 0.1, 0.01, 0.001, 0.0001)  # in m
+range_energies = np.array([300, 500, 1000, 1500, 2000, 3000, 4000])  # in keV
+range_step_sizes = (1,)  # in m
 
 mats = {
     "ge": 5.323,  # g/cm3, room temperature
@@ -47,8 +45,8 @@ def read_estar(mat: str):
     values = np.array([list(map(float, line.split())) for line in data_lines])
 
     # Convert to an Awkward Array
-    a = ak.Array({col: values[:, i] for i, col in enumerate(columns)})[:43]
-    a["x"] = 1000 * a.kinetic_energy  # in mm
+    a = ak.Array({col: values[:, i] for i, col in enumerate(columns)})[:60]
+    a["x"] = 1000 * a.kinetic_energy  # in keV
     return a
 
 
@@ -86,9 +84,14 @@ def calc_stopping_power(filename):
     stp_len = np.sqrt(
         ak_diff(data.xloc) ** 2 + ak_diff(data.yloc) ** 2 + ak_diff(data.zloc) ** 2
     )
+    assert ak.all(stp_len > 0)
+    assert not ak.any(np.isnan(data.edep[..., 0]))
 
     # convert to keV / mm
-    return data.edep[..., 0] / stp_len / 1000
+    stop = data.edep[..., 0] / stp_len / 1000
+    assert not ak.any(np.isnan(stop))
+
+    return stop
 
 
 # tests
@@ -98,12 +101,15 @@ def calc_stopping_power(filename):
 def test_electron_range_distributions(mat: str):
     fig, ax = plt.subplots()
 
-    for energy in [300, 500, 1000, 1500, 2000, 3000, 4000]:  # in keV
-        eranges = calc_range(f"electrons-{mat}-{energy}-keV.lh5")
-        # convert to mm
-        h = hist.new.Reg(200, 0, 7, name="Electron integrated path [mm]").Double()
-        h.fill(eranges)
-        h.plot(ax=ax, yerr=False, label="$E^{kin}_e$" + f" = {energy} keV")
+    for step_size in range_step_sizes:  # in m
+        for energy in range_energies:  # in keV
+            eranges = calc_range(
+                f"output/electrons-range-{mat}-{energy}-keV-{step_size}-m.lh5"
+            )
+            # convert to mm
+            h = hist.new.Reg(200, 0, 7, name="Electron integrated path [mm]").Double()
+            h.fill(eranges)
+            h.plot(ax=ax, yerr=False, label="$E^{kin}_e$" + f" = {energy:.1f} keV")
 
     ax.set_ylabel("Density")
     ax.legend()
@@ -114,26 +120,61 @@ def test_electron_range_distributions(mat: str):
 def test_electron_range_vs_energy(mat: str, density: float, estar_data: ak.Array):
     fig, ax = plt.subplots()
 
-    ax.plot(
-        estar_data.x,
-        10 * estar_data.csda_range / density,  # convert to mm
-        label="CSDA range (ESTAR)",
-    )
+    combined_e = []
+    combined_range = []
+    combined_range_err = []
+    combined_step = []
+    for step_size in step_sizes:  # in m
+        combined_e.extend(energies)
+        combined_step.extend([step_size] * len(energies))
+        for e in energies:
+            rn = calc_range(f"output/electrons-{mat}-{e}-keV-{step_size}-m.lh5")
+            combined_range.append(ak.mean(rn))
+            combined_range_err.append(ak.std(rn) / np.sqrt(ak.count(rn)))
 
-    ax.plot(
-        energies,
-        [ak.mean(calc_range(f"electrons-{mat}-{e}-keV.lh5")) for e in energies],
-        c="black",
-        marker="o",
-        lw=0,
-        ms=3,
-        label=f"$remage$ simulation: $e^-$ in {mat.title()}",
-    )
+    combined_e = np.array(combined_e)
+    combined_range = np.array(combined_range)
+    combined_range_err = np.array(combined_range_err)
+    combined_step = np.array(combined_step)
 
-    ax.set_xlim(0, 5000)
+    # inset axis.
+    axin = ax.inset_axes([0.5, 0.1, 0.45, 0.45])
+
+    for a in (axin, ax):
+        a.plot(
+            estar_data.x,
+            10 * estar_data.csda_range / density,  # convert to mm
+            label="CSDA range (ESTAR)",
+        )
+        sc = a.scatter(
+            combined_e,
+            combined_range,
+            c=combined_step,
+            marker="o",
+            s=3**2,
+            label=f"$remage$ simulation: $e^-$ in {mat.title()}",
+            norm=matplotlib.colors.LogNorm(),
+        )
+        ax.errorbar(
+            combined_e,
+            combined_range,
+            yerr=combined_range_err,
+            color="black",
+            linestyle="",
+            zorder=0,
+        )
+
+    x_max = 4000
+    axin.set_xlim(-0.1 * x_max, 1.1 * x_max)
+    y_max = np.max(combined_range[combined_e < x_max])
+    axin.set_ylim(-0.1 * y_max, 1.1 * y_max)
+
+    plt.colorbar(sc, ax=ax, label="maximum step length [m]")
+
     ax.set_xlabel("Electron kinetic energy [keV]")
     ax.set_ylabel("Electron integrated path [mm]")
-    ax.legend()
+    leg = ax.legend()
+    leg.legend_handles[1].set(array=None, ec="black", fc="black")
     ax.grid()
     fig.savefig(f"e-range-{mat}-vs-estar.output.png")
 
@@ -163,22 +204,44 @@ def test_electron_stopping_power_vs_energy(
         label="Total stopping power (ESTAR)",
     )
 
-    ax.plot(
-        energies,
-        [
-            ak.mean(calc_stopping_power(f"electrons-{mat}-{e}-keV.lh5"))
-            for e in energies
-        ],
-        c="black",
+    combined_e = []
+    combined_stop = []
+    combined_step = []
+    combined_stop_err = []
+    for step_size in step_sizes:  # in m
+        combined_e.extend(energies)
+        combined_step.extend([step_size] * len(energies))
+        for e in energies:
+            sp = calc_stopping_power(
+                f"output/electrons-{mat}-{e}-keV-{step_size}-m.lh5"
+            )
+            combined_stop.append(ak.mean(sp))
+            combined_stop_err.append(ak.std(sp) / np.sqrt(ak.count(sp)))
+
+    sc = ax.scatter(
+        combined_e,
+        combined_stop,
+        c=combined_step,
         marker="o",
-        lw=0,
-        ms=3,
+        s=3**2,
         label=f"$remage$ simulation: $e^-$ in {mat.title()}",
+        norm=matplotlib.colors.LogNorm(),
+        zorder=100,
     )
+    ax.errorbar(
+        combined_e,
+        combined_stop,
+        yerr=combined_stop_err,
+        color="black",
+        linestyle="",
+        zorder=0,
+    )
+    plt.colorbar(sc, ax=ax, label="maximum step length [m]")
 
     ax.set_xlabel("Electron kinetic energy [keV]")
     ax.set_ylabel("Stopping power [keV / mm]")
-    ax.legend()
+    leg = ax.legend()
+    leg.legend_handles[3].set(array=None, ec="black", fc="black")
     ax.grid()
     ax.set_xscale("log")
 
